1774389587ef524a2775b09b3d516228645a5847
Unknown <awned@hotmail.fr>
2018-10-25 00:23:07 +0200

    Matching system en cours

--- Assets/Scripts/AlienBehaviour.cs
+++ Assets/Scripts/AlienBehaviour.cs
@@ -17,28 +17,40 @@ public class AlienBehaviour : MonoBehaviour
 
     private void Update()
     {
-        Debug.DrawRay(transform.position, Vector3.right, 1);
+        
+
+
+        /*Debug.DrawRay(transform.position, Vector3.right, Color.green,1);
+        Debug.DrawRay(transform.position, Vector3.left, Color.blue, 1);
         if (Physics.Raycast(transform.position, Vector3.right, 1f))
         {
             rightFree = false;
+        }
+        else
+        {
+            rightFree = true;
         }
         if (Physics.Raycast(transform.position, Vector3.left, 1f))
         {
             leftFree = false;
         }
+        else
+        {
+            leftFree = true;
+        }
         if (stacked)
         {
-            if (transform.position.x - 1 < playerTrans.position.x && rightFree)
+            if (transform.position.x + 1 < playerTrans.position.x && rightFree)
             {
                 transform.position += Vector3.right;
             }
-            if (transform.position.x - 1 < playerTrans.position.x && leftFree)
+            if (transform.position.x - 1 > playerTrans.position.x && leftFree)
             {
                 transform.position += Vector3.left;
             }
-        }
+        }*/
     }
-
+    /*
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -55,5 +67,18 @@ public class AlienBehaviour : MonoBehaviour
         Destroy(obj);
         yield return new WaitForSecondsRealtime(1);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }*/
+
+    private List<GameObject> FindMatch(Vector2 castDir)
+    {
+        List<GameObject> matchingAliens = new List<GameObject>();
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir);
+        while (hit.collider != null && hit.collider.gameObject.tag == "Stack")
+        {
+            matchingAliens.Add(hit.collider.gameObject);
+            hit = Physics2D.Raycast(hit.collider.transform.position, castDir);
+        }
+        return matchingAliens;
     }
+
 }
