510b8830accfb58a23ea25fa421aadc1c87be72f
NDeev <awned@hotmail.fr>
2019-01-10 18:50:24 +0100

    Merge à la main

---
+++ Assets/Scripts/ListObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Cube
+{
+    public float posX;
+    public float posY;
+    
+}
+
+[CreateAssetMenu]
+public class ListObject : ScriptableObject
+{
+    public List<Cube> cubes;
+}
