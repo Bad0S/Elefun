056bccabf9bc9f989d27f716f922608ddfc85d93
Unknown <awned@hotmail.fr>
2019-01-16 00:57:58 +0100

    Différents background intégrés

--- Assets/Scripts/ScrollSprite.cs
+++ Assets/Scripts/ScrollSprite.cs
@@ -9,11 +9,29 @@ public class ScrollSprite : MonoBehaviour
     public GameObject enviro2;
     private Transform enviroTrans1;
     private Transform enviroTrans2;
+    private SpriteRenderer enviroRend1;
+    private SpriteRenderer enviroRend2;
+    public Sprite enviroGreen;
+    public Sprite enviroRed;
+    public Sprite enviroBlue;
 
 	void Start ()
     {
         enviroTrans1 = enviro1.GetComponent<Transform>();
         enviroTrans2 = enviro2.GetComponent<Transform>();
+        enviroRend1 = enviro1.GetComponent<SpriteRenderer>();
+        enviroRend2 = enviro2.GetComponent<SpriteRenderer>();
+
+        int a = Random.Range(0, 3);
+        switch (a)
+        {
+            case 0: enviroRend1.sprite = enviroRed; break;
+            case 1: enviroRend1.sprite = enviroGreen; break;
+            case 2: enviroRend1.sprite = enviroBlue; break;
+        }
+
+        enviroRend2.sprite = enviroRend1.sprite;
+
     }
 	
 	// Update is called once per frame
